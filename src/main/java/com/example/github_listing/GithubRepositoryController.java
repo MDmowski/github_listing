59b711b89b85979c7c44e6fc2e214f29c7d9ad97
Maciej Dmowski <maciej.dmowski@icloud.com>
2021-04-23 18:14:44 +0200

    Add all possible urls listing

--- src/main/java/com/example/github_listing/GithubRepositoryController.java
+++ src/main/java/com/example/github_listing/GithubRepositoryController.java
@@ -1,20 +1,35 @@
 package com.example.github_listing;
 
+import net.minidev.json.JSONObject;
 import org.springframework.http.*;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 
 @RestController
 @Validated
 public class GithubRepositoryController {
+    HashMap<String,String> urls;
+    GithubRepositoryController(){
+        urls = new HashMap<>();
+        urls.put("list", "/list?username={user}");
+        urls.put("stars", "/stars?username={user}");
+    }
+    @RequestMapping("/")
+    public ResponseEntity<Map<String,String>> index() {
+        return new ResponseEntity<>(urls, HttpStatus.OK);
+    }
+
     @GetMapping("/list")
     public ResponseEntity<GithubRepository[]> listRepositories(@RequestParam(value = "username") String username) {
         return getResponseEntity(username);
